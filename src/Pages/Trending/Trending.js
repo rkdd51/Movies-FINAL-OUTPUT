02a79531ea27f8825e229096d088b356b5b7021c
rkdd51 <rahuldubay51@gmail.com>
2021-06-07 12:51:25 +0530

    imdb

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import axios from "axios";
+import "./Trending.css";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
 
 const Trending = () => {
-    return (
-        <div>
-        Trending
-            
-        </div>
-    )
-}
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
 
-export default Trending
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+      
+      // "https://api.themoviedb.org/3/trending/all/week?api_key=7b19ac55c37eac5ebc9c408117ee9e11"
+    );
+
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchTrending();
+    
+  }, [page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Trending Today</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      <CustomPagination setPage={setPage} />
+    </div>
+  );
+};
+
+export default Trending;
